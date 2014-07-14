a25afb1f98134091d294402c88e56b1a821ce1e7
Tong Wu <tong.wu@live.com>
2014-07-14 15:38:31 +0800

    first version

---
+++ SK.Data.Pipeline.Core/Common/Logger.cs
@@ -0,0 +1,28 @@
+﻿using log4net;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SK.Data.Pipeline.Core.Common
+{
+    public class Logger
+    {
+        private static ILog _Log;
+
+        public static ILog Instance
+        {
+            get
+            {
+                if (_Log == null)
+                {
+
+                    _Log = LogManager.GetLogger("SK.Data.Pipeline.Logger");
+                }
+
+                return _Log;
+            }
+        }
+    }
+}
---
+++ SK.Data.Pipeline.Core/Common/StringUtil.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SK.Data.Pipeline.Core.Common
+{
+    public static class StringUtil
+    {
+        public static void Info(this string str)
+        {
+            Logger.Instance.Info(str);
+        }
+
+        public static void Warning(this string str)
+        {
+            Logger.Instance.Warn(str);
+        }
+
+        public static void Error(this string str)
+        {
+            Logger.Instance.Error(str);
+        }
+
+        public static object AutoConvertToObject(this string valueStr)
+        {
+            if (valueStr == null) return null;
+
+            int intObj = 0;
+            if (int.TryParse(valueStr, out intObj))
+            {
+                return intObj;
+            }
+
+            return valueStr;
+        }
+    }
+}
